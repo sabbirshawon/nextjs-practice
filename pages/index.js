d88789e8c748604326dca2317a1d6e822f30362a
sabbirshawon <sabbirs93@gmail.com>
2021-09-08 22:41:02 +0600

    code re-factoring and removed unused codes

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,7 @@ const Cell = ({ x, y, type }) => {
   );
 };
 
+// generate new food
 const getRandomCell = () => ({
   x: Math.floor(Math.random() * Config.width),
   y: Math.floor(Math.random() * Config.width),
@@ -69,17 +70,14 @@ const Snake = () => {
     { x: 7, y: 12 },
     { x: 6, y: 12 },
   ];
-  const grid = useRef();
 
   // snake[0] is head and snake[snake.length - 1] is tail
   const [snake, setSnake] = useState(getDefaultSnake());
   const [direction, setDirection] = useState(Direction.Right);
 
-  const [food, setFood] = useState({ x: 4, y: 10 });
   const [score, setScore] = useState(0);
-  const [foodArr, setFoodArr] = useState([{x: 4, y: 10}])
-
-  const multipleFood = () => setFoodArr(prev => [...prev, getRandomCell()])
+  // food array
+  const [foodArr, setFoodArr] = useState([{ x: 4, y: 10 }]);
 
   // move the snake
   useEffect(() => {
@@ -103,72 +101,70 @@ const Snake = () => {
     const timer = setInterval(runSingleStep, 500);
 
     return () => clearInterval(timer);
-  }, [direction, food]);
+  }, [direction]);
 
   // update score whenever head touches a food
   useEffect(() => {
-
     setTimeout(() => {
-      
-    const head = snake[0];
-    const tail = snake.slice(0);
-    //task 1-2
-    for(var i = 3; i < snake.length; i++){
-      if(
-          head.x === tail[i].x && head.x !== 1 && head.x !== 23 &&
-          head.y === tail[i].y && head.y !== 1 && head.y !== 23
-      ) {
-        // console.log('hit')
-        // console.log(head.x, head.y, tail[i].x, tail[i].y)
-        window.location.reload()
+      // head of snake means first cell of green
+      const head = snake[0];
+      // tail of snake mean remaining cells of snake which are in green
+      const tail = snake.slice(0);
+      // task 1-2
+      // checking snake collision with itself
+      for (var i = 3; i < snake.length; i++) {
+        if (
+          head.x === tail[i].x &&
+          head.x !== 1 &&
+          head.x !== 23 &&
+          head.y === tail[i].y &&
+          head.y !== 1 &&
+          head.y !== 23
+        ) {
+          // reload the page
+          window.location.reload();
+        }
       }
-    } 
-    if (isFoodArr(head)) {
-      // task 1-1
-      setSnake((prev) => {
-        return [...prev, { x: 6, y: 12 }];
-      });
-      setScore((score) => {
-        return score + 1;
-      });
-
-      let newFood = getRandomCell();
-    
-      while (isSnake(newFood)) {
-        newFood = getRandomCell();
-        // setFoodArr(prev => [...prev].slice(1))
+      // if snake eats food / collided its head with food
+      if (isFoodArr(head)) {
+        // task 1-1
+        // increase snake size
+        setSnake((prev) => {
+          return [...prev, { x: 6, y: 12 }];
+        });
+        // increase score by 1
+        setScore((score) => {
+          return score + 1;
+        });
+        // remove the food which has been eaten by snake
+        setFoodArr((prev) =>
+          [...prev].filter((filt) => head.x !== filt.x && head.y !== filt.y)
+        );
       }
-      // setFood(newFood)
-      // task 1-4
-     
-       setFoodArr(prev => [...prev].filter(filt => head.x !== filt.x && head.y !== filt.y))
-      //  setFoodArr(prev => {
-      //    return [...prev, newFood].length > 10 ? [...prev].splice(10, 1) : [...prev, newFood]
-      //  })
-    }
-  }, 500);
-    
+    }, 500);
+    // this useEffect will run again when snake value changes
   }, [snake]);
-  
+
   // task 1-4
   useEffect(() => {
+    // generate new food after every 3 secs
     const interval = window.setInterval(() => {
-    let newFood = getRandomCell();
-    setFoodArr((prev) => [
-      ...prev,
-      newFood
-    ])
-  }, 3000)
+      let newFood = getRandomCell();
+      setFoodArr((prev) => [...prev, newFood]);
+    }, 3000);
 
-  const removeAfter10 = window.setInterval(() => {
-    setFoodArr(prev => [...prev].slice(1))
-  }, 10000)
+    // remove food after every 10 secs
+    const removeAfter10 = window.setInterval(() => {
+      setFoodArr((prev) => [...prev].slice(1));
+    }, 10000);
     return () => {
-      clearInterval(interval)
-      clearInterval(removeAfter10)
-    }
-}, [])
-  
+      // clean up
+      clearInterval(interval);
+      clearInterval(removeAfter10);
+    };
+  }, []);
+
+  // default direction
   let key = "ArrowRight";
 
   useEffect(() => {
@@ -176,32 +172,37 @@ const Snake = () => {
       // task 1-2
       switch (event.key) {
         case "ArrowUp":
-          if(key === "ArrowDown"){
-            return
+          // check if key is equal to ArrowDown then it will not move in opposite direction which
+          // in this case is ArrowUp
+          if (key === "ArrowDown") {
+            return;
           }
           setDirection(Direction.Top);
           key = event.key;
           break;
 
         case "ArrowDown":
-          if(key === "ArrowUp"){
-            return
+          // same logic as the above
+          if (key === "ArrowUp") {
+            return;
           }
           setDirection(Direction.Bottom);
           key = event.key;
           break;
 
         case "ArrowLeft":
-          if(key === "ArrowRight"){
-            return
+          // same logic as the above
+          if (key === "ArrowRight") {
+            return;
           }
           setDirection(Direction.Left);
           key = event.key;
           break;
 
         case "ArrowRight":
-          if(key === "ArrowLeft"){
-            return
+          // same logic as the above
+          if (key === "ArrowLeft") {
+            return;
           }
           setDirection(Direction.Right);
           key = event.key;
@@ -214,55 +215,72 @@ const Snake = () => {
   }, []);
 
   // task 1-3
+  // well i didnt think of any good name sorry for that
+  // it detects border collision with snake
   const someFunc = (index, sign, y) => {
-     if(!y) {
+    // it is checking y param is empty then its x axis so this check will work otherwise the
+    // else check will be executed which is checking y value
+    if (!y) {
       let ind = index;
-        const arr = snake;
-        const updatedArray = arr.map(sna => {
-          return {...sna, x: sign ? ind + 1 : ind - 1}
-        })
-        setTimeout(() => {
-          setSnake(updatedArray)
-        }, 300);
-        
-      } else {
-        let ind = index;
-        const arr = snake;
-        const updatedArray = arr.map(sna => {
-          return {...sna, y: sign === "+" ? ind + 1 : ind - 1}
-        })
-        setTimeout(() => {
-          setSnake(updatedArray)
-        }, 300);
-      }
-
-  }
+      const arr = snake;
+      const updatedArray = arr.map((sna) => {
+        // if x = 0 then i changed index to 24 which is last cell in the opposite direction
+        // and vice versa
+        // if x === 0 then i am incrementing otherwise decrementing
+        return { ...sna, x: sign ? ind + 1 : ind - 1 };
+      });
+      setTimeout(() => {
+        setSnake(updatedArray);
+      }, 300);
+    } else {
+      let ind = index;
+      const arr = snake;
+      const updatedArray = arr.map((sna) => {
+        // if y === 0 then i am incrementing otherwise decrementing
+        return { ...sna, y: sign === "+" ? ind + 1 : ind - 1 };
+      });
+      setTimeout(() => {
+        setSnake(updatedArray);
+      }, 300);
+    }
+  };
   // ?. is called optional chaining
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-  const isFood = ({ x, y }) => food?.x === x && food?.y === y;
-  const isFoodArr = ({x, y}) => foodArr.find(f => f.x === x && f.y === y)
-  
+
+  // checking if cell is food or not
+  const isFoodArr = ({ x, y }) => foodArr.find((f) => f.x === x && f.y === y);
+
+  // checking if cell is snake
   const isSnake = ({ x, y }) => {
-    return snake.find((position) => position.x === x && position.y === y)
-  }
-    
+    return snake.find((position) => position.x === x && position.y === y);
+  };
+
   const cells = [];
-  
+
+  // setting cells based on width and height
   for (let x = 0; x < Config.width; x++) {
     for (let y = 0; y < Config.height; y++) {
       let type = CellType.Empty;
-       // task 1-4
-      if (isFoodArr({x, y})) {
+      // task 1-4
+      if (isFoodArr({ x, y })) {
         type = CellType.Food;
-      } 
-      else if (isSnake({ x, y })) {
+      } else if (isSnake({ x, y })) {
         type = CellType.Snake;
         // task 1-3
-         x === 24 ? someFunc(0, "+", '') : x === 0 ? 
-         someFunc(24, "", '') : y === 24 ? 
-         someFunc(0, "+", "y") : y === 0 ? 
-         someFunc(24, "-", "y") : ''
-      } 
+        // if cell has 24 x value then i am changing its value to 0 the opposite cell in the
+        // row and vice wersa
+        // if cell has 24 y value then i am changing its value to 0 the opposite cell in the
+        // column and vice wersa
+        x === 24
+          ? someFunc(0, "+", "")
+          : x === 0
+          ? someFunc(24, "", "")
+          : y === 24
+          ? someFunc(0, "+", "y")
+          : y === 0
+          ? someFunc(24, "-", "y")
+          : "";
+      }
       cells.push(<Cell key={`${x}-${y}`} x={x} y={y} type={type} />);
     }
   }
@@ -275,7 +293,6 @@ const Snake = () => {
         Score: {score}
       </div>
       <div
-      ref={grid}
         className={styles.grid}
         style={{
           height: Config.height * Config.cellSize,
